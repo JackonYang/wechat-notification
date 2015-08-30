acb20229d04b07638095f9e1cef3b324862c4cfa
Jackon <jiekunyang@gmail.com>
2015-08-30 17:30:20 +0800

    listen and send message by http-GET

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # wechat-notification
-通过微信公众号, 将通知信息推送至个人微信. 无需认证公众号, 可群发.
+
+通过微信公众号, 将通知消息推送至个人微信. 无需认证公众号, 可群发.
+
+## DEMO
+
+demo 环境的公众号
+
+![qrcode](doc/qrcode.jpg)
+
+关注后发送 `dy` 2 个英文字母, `订阅`消息推送.
+
+通过 [http://safebang.org/wechat-api/send?msg=hello](http://safebang.org/wechat-api/send?msg=hello), 推送消息.
+
+其中, hello 为要推送的消息
+
+
+
+## 服务器部署
+
+#### clone 并安装依赖
+
+```shell
+$ git clone git@github.com:JackonYang/wechat-notification.git
+$ cd wechat-notification
+$ sudo pip install -r requirements.txt
+```
+
+#### 启动服务器
+
+微信要求走 80 端口, linux 监听 80 端口需要 root 权限.
+
+```shell
+$ sudo python app.py --port=80 --username=xxxx --password=xxxx --token=xxxx
+```
+
+其中, 
+
+username 和 password 是微信公众号的用户名和密码.
+
+token 是 `开发者中心-服务器配置` 中设置的 token
+
+![token 配置](doc/token_config.png)
+
+
+## 自定义操作指令
+
+默认指令:
+
+- dy: 订阅
+- td: 退订
+- ls: 已订阅用户列表
+- help: 帮助文档
+
+在 CmdRoot 类中增加 classmethod 方法即可.
+
+方法名必须以 `cmd_` 为前缀, 依次接收 fakeid, user(nickname) 两个参数.
+例如, `dy` 命令对应的 `cmd_dy` 定义如下:
+
+```python
+class CmdRobot:
+    @classmethod
+    def cmd_dy(self, fakeid, user):
+        user_fakeid[user] = fakeid
+        return 'subscribed. nickname={}, fakeid={}'.format(user, fakeid)
+```
+
+## 本地调试
+
+每次都在 server 上进行代码调试, 不方便.
+可以通过 nginx 转发, 实现本地调试.
+
+nginx 参考配置如下:
+
+```shell
+upstream tornadoes {
+    server 106.44.127.160:8002;  # 开发环境 IP: port
+}
+
+server {
+    listen 80;
+    server_name safebang.org www.safebang.org;
+
+    root /home/jackon/safebang;
+    index index.html index.htm;
+
+    location / {
+        proxy_pass_header Server;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Scheme $scheme;
+        proxy_pass http://tornadoes;
+    }
+}
+```
+
+其中, `server 106.44.127.160:8002` 改为真实的 IP, 端口即可.
+
+开发环境启动服务器, 监听 8002 端口.
+```shell
+$ python app.py --port=8002 --username=xxxx --password=xxxx --token=xxxx
+```
---
+++ app.py
@@ -0,0 +1,146 @@
+# -*- Encoding: utf-8 -*-
+import json
+import time
+from os.path import join, dirname, exists
+from os import makedirs
+
+import tornado.ioloop
+import tornado.web
+from tornado.options import define, options
+
+from wechat_sdk import WechatBasic, WechatExt
+from wechat_sdk.exceptions import NeedLoginError
+
+define("username", default='username', help="username of wechat", type=str)
+define("password", default='password', help="password of wechat", type=str)
+define("token", default='', help="token of wechat", type=str)
+define("port", default=8000, help="run on the given port", type=int)
+define("debug", default=False, help="run in Debug mode", type=bool)
+
+
+today = lambda: time.strftime('%Y%m%d', time.localtime())
+cookie_dir = join(dirname(__file__), 'cookie')
+
+
+def login_http(username, password):
+    wechat = WechatExt(username, password)
+    wechat.login()
+
+    if not exists(cookie_dir):
+        makedirs(cookie_dir)
+    fn = join(cookie_dir, 'cookie_{}.html'.format(today()))
+    with open(fn, 'w') as f:
+        json.dump(wechat.get_token_cookies(), f, indent=4)
+
+    return wechat
+
+
+def login_cookie(username, password):
+    fn = join(cookie_dir, 'cookie_{}.html'.format(today()))
+    if not exists(fn):
+        raise NeedLoginError
+
+    with open(fn, 'r') as f:
+        kwargs = json.load(f)
+
+    return WechatExt(username=username, password=password,
+                     login=False, **kwargs)
+
+
+def action(username, password, meth_name, *args, **kwargs):
+    try:
+        wechat = login_cookie(username, password)
+        ret = getattr(wechat, meth_name)(*args, **kwargs)
+    except NeedLoginError:
+        wechat = login_http(username, password)
+        ret = getattr(wechat, meth_name)(*args, **kwargs)
+
+    return ret
+
+
+user_fakeid = dict()
+
+
+class CmdRobot:
+    @classmethod
+    def cmd_dy(self, fakeid, user):
+        user_fakeid[user] = fakeid
+        return 'subscribed. nickname={}, fakeid={}'.format(user, fakeid)
+
+    @classmethod
+    def cmd_td(self, fakeid, user):
+        if user in user_fakeid:
+            user_fakeid.pop(user)
+        return 'unsubscribe successfully. nickname={}'.format(user)
+
+    @classmethod
+    def cmd_ls(self, fakeid, user):
+        if user_fakeid:
+            return 'user list:\n{}'.format('\n'.join(user_fakeid))
+        else:
+            return 'no user subscribed'
+
+    @classmethod
+    def cmd_help(self, fakeid, user):
+        return u'回复: \ndy -- 订阅\ntd -- 退订\nls -- 已订阅用户列表\nhelp -- 帮助文档'
+
+
+class WechatListener(tornado.web.RequestHandler):
+
+    def auth(self):
+        args = ['signature', 'timestamp', 'nonce']
+        kwargs = {k: self.get_argument(k, '') for k in args}
+
+        wechat = WechatBasic(token=options.token)
+        if not wechat.check_signature(**kwargs):
+            self.set_status(403)
+            self.write('auth failed')
+            self.finish()
+
+    def get(self):
+        """auth
+
+        """
+        self.auth()
+
+        self.write(self.get_argument('echostr', 'echostr not found'))
+
+    def post(self):
+        self.auth()
+
+        raw_msg = action(options.username, options.password, 'get_top_message')
+        msg = json.loads(raw_msg)['msg_item'][0]
+        content, fakeid, user = msg['content'], msg['fakeid'], msg['nick_name']
+        cmd = getattr(CmdRobot, 'cmd_{}'.format(content.encode('utf8')), CmdRobot.cmd_help)
+
+        wechat_basic = WechatBasic(token=options.token)
+        wechat_basic.parse_data(self.request.body)
+
+        self.write(wechat_basic.response_text(cmd(fakeid, user)))
+
+
+class WechatSender(tornado.web.RequestHandler):
+
+    def get(self):
+        msg = self.get_argument('msg')
+
+        for fakeid in user_fakeid.values():
+            print fakeid
+            action(options.username, options.password, 'send_message', fakeid, msg)
+        self.write('{} msg sent'.format(len(user_fakeid)))
+
+
+def main():
+    tornado.options.parse_command_line()
+
+    application = tornado.web.Application([
+        (r"/wechat-api/listen", WechatListener),
+        (r"/wechat-api/send", WechatSender),
+        ],
+        debug=options.debug)
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
